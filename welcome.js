307c839f4a546ce7e7b045e390cb03a77bfc0e8f
Lakshin_Pathak <145486751+LakshinPathak@users.noreply.github.com>
2024-07-12 14:07:57 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,2 @@
+console.log("Welcome to Your Social Media App! We're thrilled to have you join our community.
+With Your Social Media App, you can connect with friends, share moments, and explore a world of possibilities.");
